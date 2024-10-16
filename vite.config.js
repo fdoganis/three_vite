70b6172c0c440de5fc09afda6659a6405981b8fd
fdoganis <fivos.doganis@gmail.com>
2024-10-16 12:45:48 +0200

    github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,38 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
+    base: "/three_vite",
     clearScreen: false,
+    optimizeDeps: {
+        esbuildOptions: {
+            supported: {
+                'top-level-await': true
+            }
+        }
+    },
+    esbuild: {
+        supported: {
+            'top-level-await': true
+        }
+    },
     build: {
-        sourcemap: true
+        sourcemap: true,
     },
     server: {
         open: true
     },
     plugins: [
         viteStaticCopy({
-          targets: [
-              { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
-	      { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' },
-	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.js', dest: 'jsm/libs/draco/gltf' },
-	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.wasm', dest: 'jsm/libs/draco/gltf/' },
-	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_encoder.js', dest: 'jsm/libs/draco/gltf/' },
-	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_wasm_wrapper.js', dest: 'jsm/libs/draco/gltf/' }
-          ]
+            targets: [
+                { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
+                { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' },
+                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.js', dest: 'jsm/libs/draco/gltf' },
+                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.wasm', dest: 'jsm/libs/draco/gltf/' },
+                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_encoder.js', dest: 'jsm/libs/draco/gltf/' },
+                { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_wasm_wrapper.js', dest: 'jsm/libs/draco/gltf/' }
+            ]
         }),
         glsl()
-      ]
+    ]
 })
 
